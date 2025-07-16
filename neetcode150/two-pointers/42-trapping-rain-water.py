7aedd8566bd5a26e5d809b245715ac5fb1249e8d
Jet Kan <jetkan.yk@gmail.com>
2025-07-16 21:28:23 +0800

    add dp solution

--- neetcode150/two-pointers/42-trapping-rain-water.py
+++ neetcode150/two-pointers/42-trapping-rain-water.py
@@ -1,16 +1,37 @@
 """
 Problem: https://leetcode.cn/problems/trapping-rain-water/
-Tags:
 Difficulty: Hard
-Time: O()
-Space: O()
-Solution:
+
+Solution #1: Dynamic Programming
+- Time: O(n)
+- Space: O(n)
+
+The dynamic programming approach pre-computes the maximum height to the left and right of each position.
+For each position, the water level is determined by the minimum of the maximum heights on both sides.
+The amount of water trapped at each position is the difference between this water level and the height at that position.
 """
 
 
 class Solution:
     def trap(self, height: list[int]) -> int:
-        pass
+        return self.dp(height)
+
+    def dp(self, height: list[int]) -> int:
+        if len(height) < 3:
+            return 0
+
+        max_left = [0] * len(height)
+        for i in range(1, len(height)):
+            max_left[i] = max(max_left[i - 1], height[i - 1])
+
+        max_right = [0] * len(height)
+        for i in range(len(height) - 2, -1, -1):
+            max_right[i] = max(max_right[i + 1], height[i + 1])
+
+        area = 0
+        for i in range(len(height)):
+            area += max(min(max_left[i], max_right[i]) - height[i], 0)
+        return area
 
 
 if __name__ == "__main__":
